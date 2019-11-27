87309e8adcd0b0b491123b57dbc1537d0f1711f9
Jorge Gonzalez <jorgegonzalez@users.noreply.github.com>
2019-11-26 23:24:44 -0500

    feat: add more rules

--- index.js
+++ index.js
@@ -6,11 +6,13 @@ module.exports = {
     'plugin:unicorn/recommended',
     'plugin:promise/recommended',
     'plugin:jest/recommended',
+    // import
+    'plugin:import/errors',
+    'plugin:import/warnings',
     // React
     'react-app',
     'plugin:react/recommended',
     // Typescript
-    // Trigger release
     'plugin:@typescript-eslint/eslint-recommended',
     'plugin:@typescript-eslint/recommended',
     // Prettier
@@ -34,15 +36,30 @@ module.exports = {
     'prettier'
   ],
   rules: {
+    'no-shadow': 'warn',
+    'prefer-destructuring': 'warn',
+    'prefer-object-spread': 'warn',
+    // import
+    'import/no-unresolved': 'error',
+    // prettier
     'prettier/prettier': 'warn',
+    // no-use-extend-native
     'no-use-extend-native/no-use-extend-native': 'error',
-    'unicorn/filename-case': 'off',
-    'unicorn/prevent-abbreviations': 'off',
+    // react
     'react-hooks/rules-of-hooks': 'error',
     'react-hooks/exhaustive-deps': 'warn',
+    'react/prop-types': 'off',
+    'react/jsx-curly-brace-presence': 'warn',
+    'react/button-has-type': 'warn',
+    'react/no-danger': 'warn',
+    // typescript
     '@typescript-eslint/no-var-requires': 'off',
     '@typescript-eslint/camelcase': 'off',
     '@typescript-eslint/explicit-function-return-type': 'off',
-    'react/prop-types': 'off'
+    // unicorn
+    'unicorn/filename-case': 'off',
+    'unicorn/prevent-abbreviations': 'off',
+    'unicorn/no-abusive-eslint-disable': 'off',
+    'unicorn/no-unsafe-regex': 'error'
   }
 };
